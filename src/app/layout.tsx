e16b58e46dfa5307e3881dd85c324c74a394b993
Palicz Dániel <hdtoki@gmail.com>
2025-02-15 15:25:29 +0100

    feat: added Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ import { TopBar } from "@/components/layout/top-bar";
 import { ScrollToTop } from "@/components/ui/scroll-to-top";
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 /**
  * Poppins Font Configuration
@@ -306,6 +307,7 @@ export default function RootLayout({
         <main className="flex-grow">{children}
           {/* Vercel Analytics component */}
         <Analytics />
+        <SpeedInsights />
         </main>
         {/* Utility component for improving UX on long pages */}
         <ScrollToTop />
